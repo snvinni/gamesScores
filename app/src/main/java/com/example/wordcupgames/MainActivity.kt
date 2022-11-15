7b1a3035232a10e37e1922215ee37d0fea182e98
vini <vinicius.nascimento@aiqfome.com>
2022-11-15 20:03:51 -0300

    first commit

---
+++ app/src/main/java/com/example/wordcupgames/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.wordcupgames
+
+import android.annotation.TargetApi
+import android.app.Activity
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setStatusBarGradiant(this)
+    }
+
+    private fun setStatusBarGradiant(activity: Activity) {
+        val window: Window = activity.window
+        val background = ContextCompat.getDrawable(activity, R.drawable.gradient)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+
+        window.statusBarColor = ContextCompat.getColor(activity,android.R.color.transparent)
+        window.setBackgroundDrawable(background)
+    }
+}
